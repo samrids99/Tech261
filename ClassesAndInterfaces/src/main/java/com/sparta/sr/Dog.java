fff63b072dec8b12faf2b74d23161c4ccf2b8501
Sam Ridsdale <samridsdale108@gmail.com>
2024-06-18 15:41:46 +0100

    classes and interfaces created

---
+++ ClassesAndInterfaces/src/main/java/com/sparta/sr/Dog.java
@@ -0,0 +1,42 @@
+package com.sparta.sr;
+
+public class Dog {
+    private String name;
+    private String breed;
+    private int age;
+
+    public Dog(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        Dog popadom = new Dog("Popadom", "Cockerpoo", 7);
+        System.out.println(popadom.getName());
+    }
+}
