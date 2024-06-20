4b101cecc5830abf0f6e8683a2f1f77da76e3b21
Sam Ridsdale <samridsdale108@gmail.com>
2024-06-20 11:01:46 +0100

    annotations

--- Strings/src/main/java/com/sparta/sr/App.java
+++ Strings/src/main/java/com/sparta/sr/App.java
@@ -12,7 +12,8 @@ public class App {
         System.out.println(string3.hashCode());
         System.out.println(string4.hashCode());
         System.out.println("");
-        System.out.println(string1.equals(string3));
+        System.out.println(string1.equals(string3)); // because of string objects overriding of .eqauls() method
+        System.out.println(string1 == string3);
         System.out.println(System.identityHashCode(string3)); // gives original hashCode based on the object rather than the string
 
     }
